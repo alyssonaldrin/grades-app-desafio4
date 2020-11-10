e78b1fd95d22c6ac45553e80e5f3736e1fa88f69
Alysson 'Inalvejavel' Aldrin <alysson_cdf@hotmail.com>
2020-11-10 17:14:08 -0300

    url right

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://backend-grades-alysson.herokuapp.com',
+  baseURL: 'https://backend-grades-alysson.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
